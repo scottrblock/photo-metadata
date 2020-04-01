93e8e8896e7d6336573cbf3d4dfff015a06bd061
Scott Block <scottrblock@gmail.com>
2020-04-01 00:45:48 -0400

    Small script to move image files, renamed with their creation date

---
+++ export_metadata.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require "date"
+require "rmagick"
+include Magick
+
+class ExportMetadata
+  SOURCE_PATH = "photos/src/"
+  EXPORT_PATH = "photos/public/"
+
+  def perform
+    Dir["#{SOURCE_PATH}/*"].each do |filename|
+      image = ImageList.new(filename)[0]
+      date_string = Date.strptime(Image.read(filename)[0]["exif:DateTimeOriginal"], '%Y:%m:%d').to_s
+      image.write("#{EXPORT_PATH}photo-#{date_string}.jpg")
+    end
+  end
+end
+
+puts ExportMetadata.new.perform
